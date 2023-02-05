1780323e37851de7947008a221db44fa699019fa
Nikita <nkuznetsov01@mail.ru>
2023-02-06 00:32:14 +0300

    main function

---
+++ src/emergency_async/mod.rs
@@ -0,0 +1,6 @@
+pub mod calls {
+    use std::path::Path;
+    pub fn call_911(path : &Path, calls_queue : Vec<String>){
+
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,23 @@
-use std::env;
-fn main() {
-    println!("Hello, world!");
-    let path = env::var("PATHIO").unwrap_or_else(|error|{
+mod emergency_async;
 
-    });
-    println!("{}", path)
+use std::{env, fs};
+use std::path::Path;
+use emergency_async::calls::call_911;
+
+const DIR_NAME : &str = "/report";
+const DEFAULT_PATH : &str = "/home/mike/rust";
+
+fn main() {
+    let path : String = env::var("PATHIO").unwrap_or_else(|_error|{
+        println!("Указатель на файл PATHIO не найден! Производится поиск файла по стандартному пути.");
+        DEFAULT_PATH.to_string()
+    }) + DIR_NAME;
+    fs::create_dir_all(&path).expect("Не удалось создать директорию для отчетов!");
+    call_911(&Path::new(&path), vec![
+        "Проспект Луначарского д. 21к1 кв. 123".to_string(),
+        "Кронверский проспект д. 24".to_string(),
+        "Улица Ломоносова д. 16".to_string(),
+        "Проспект Луначарского 21к1".to_string(),
+        "Биржевая линия д. 12".to_string(),
+    ]);
 }
