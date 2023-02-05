f1cc56da9cea9f0b5649d02ed14e94ce4c89fb74
Nikita <nkuznetsov01@mail.ru>
2023-02-06 02:18:46 +0300

    some problems with lifetime

--- src/emergency_async/mod.rs
+++ src/emergency_async/mod.rs
@@ -1,6 +1,25 @@
 pub mod calls {
     use std::path::Path;
-    pub fn call_911(path : &Path, calls_queue : Vec<String>){
+    use futures::stream::{StreamExt, self};
 
+    pub async fn call_911(path: &Path, calls_queue: Vec<String>, thread_count: Option<usize>) {
+        let prepared: Vec<Call> = (1..=calls_queue.len()).map(
+            |index| Call{
+                address: calls_queue[index].as_str(), id:
+                index as u32}
+        ).collect();
+        let stream = stream::iter(prepared);
+        stream.for_each_concurrent(
+            thread_count.unwrap_or(3), // has 3 brigades by default
+            |call| async move {
+                send_brigade(path, call).await
+            }).await;
+    }
+
+    async fn send_brigade(path: &Path, call: Call) {}
+
+    struct Call<'a> {
+        id: u32,
+        address: &'a str,
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,29 @@ mod emergency_async;
 use std::{env, fs};
 use std::path::Path;
 use emergency_async::calls::call_911;
+use futures::executor::block_on;
 
-const DIR_NAME : &str = "/report";
-const DEFAULT_PATH : &str = "/home/mike/rust";
+const DIR_NAME: &str = "/report";
+const DEFAULT_PATH: &str = "/home/mike/rust";
+const BRIGADES_COUNT: usize = 3;
 
 fn main() {
-    let path : String = env::var("PATHIO").unwrap_or_else(|_error|{
-        println!("Указатель на файл PATHIO не найден! Производится поиск файла по стандартному пути.");
+    let path: String = env::var("ROOT_PATH_IO").unwrap_or_else(|_error| {
+        println!("Указатель на файл ROOT_PATH_IO не найден! Производится поиск файла по стандартному пути.");
         DEFAULT_PATH.to_string()
     }) + DIR_NAME;
+
     fs::create_dir_all(&path).expect("Не удалось создать директорию для отчетов!");
-    call_911(&Path::new(&path), vec![
-        "Проспект Луначарского д. 21к1 кв. 123".to_string(),
-        "Кронверский проспект д. 24".to_string(),
-        "Улица Ломоносова д. 16".to_string(),
-        "Проспект Луначарского 21к1".to_string(),
-        "Биржевая линия д. 12".to_string(),
-    ]);
+    println!("Директория для отчетов была удачно создана");
+
+    block_on(call_911(
+        &Path::new(&path),
+        vec![
+            "Проспект Луначарского д. 21к1 кв. 123".to_string(),
+            "Кронверкский проспект д. 24".to_string(),
+            "Улица Ломоносова д. 16".to_string(),
+            "Проспект Луначарского 21к1".to_string(),
+            "Биржевая линия д. 12".to_string(),
+        ],
+        Some(BRIGADES_COUNT)));
 }
