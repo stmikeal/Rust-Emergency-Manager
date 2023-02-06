3188cb36918d794efea8546a0c3db4f05be97852
Nikita <nkuznetsov01@mail.ru>
2023-02-06 03:17:38 +0300

    version 0.1

--- src/emergency_async/mod.rs
+++ src/emergency_async/mod.rs
@@ -7,23 +7,33 @@ pub mod calls {
     use tokio_stream;
     use tokio::time::{sleep, Duration};
 
-    pub async fn call_911(path: &Path, calls_queue: Vec<String>, thread_count: Option<usize>) {
+    pub async fn call_911(path: &String, calls_queue: Vec<String>, thread_count: Option<usize>) {
         let prepared: Vec<Call> = (1..calls_queue.len()).map(
             |index| Call{
                 address: calls_queue[index].as_str(), id:
                 index as u32}
         ).collect();
-        let mut stream = tokio_stream::iter(prepared);
+        let stream = tokio_stream::iter(prepared);
+        let len = calls_queue.len();
         stream.for_each_concurrent(
             thread_count.unwrap_or(3), // has 3 brigades by default
             |call| async move {
-                send_brigade(path, call).await
+                send_brigade(path, len, call).await
             }).await;
     }
 
-    async fn send_brigade(path: &Path, call: Call<'_>) {
-        sleep(Duration::from_secs(5-call.id as u64)).await;
-        echo()
+    async fn send_brigade(path: &String, len: usize, call: Call<'_>) {
+        let duration = len as u64 - call.id as u64;
+        sleep(Duration::from_secs(duration)).await; // for demonstration purpose
+        echo(format!(
+            "Зафиксирован выезд:\n\
+            Адрес дома: {}\n\
+            Номер вызова за сегодня: {}\n\
+            Выполнен за: {} единиц времени",
+            call.address, call.id, duration).as_str(),
+             Path::new(&format!("{}/{}.report", path, call.id)))
+            .expect(format!("Не удалось записать в файл номер {}", call.id).as_str());
+        println!("Адрес: {}, Номер: {}", call.address, call.id); // for demonstration purpose
     }
 
     fn echo(s: &str, path: &Path) -> io::Result<()> {
